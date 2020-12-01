4189280341a414d04ff17168144d9dd174a152d0
Dean <lynchdean@gmail.com>
2020-12-01 17:15:10 +0000

    Homepage content

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,42 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import { Navbar, Nav, Container } from "react-bootstrap";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import brand from "./assets/brand.png";
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Navbar bg="dark" variant="dark">
+        <Navbar.Brand href="/">
+          <img
+            alt="BirdWatch Fingal"
+            src={brand}
+            width="200"
+            className="d-inline-block align-top my-2 mr-4"
+          />{" "}
+        </Navbar.Brand>
+        <Nav className="mr-auto">
+          <Navbar.Text className="mr-2">
+            <Link to="/">Home</Link>
+          </Navbar.Text>
+          <Navbar.Text className="mr-2">
+            <Link to="/about">About</Link>
+          </Navbar.Text>
+        </Nav>
+      </Navbar>
+
+      <Container>
+        <Switch>
+          <Route path="/about">
+            <About />
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+      </Container>
+    </Router>
   );
 }
-
-export default App;
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,44 +1,171 @@
 import React from "react";
-import { Card, Jumbotron } from "react-bootstrap";
+import { Alert, Button, Card, CardColumns, Jumbotron } from "react-bootstrap";
+import { Timeline } from "react-twitter-widgets";
+import FingalLocation from "../assets/fingal-location.png";
+import Rogerstown from "../assets/rogerstown.jpg";
+import MemberLogo from "../assets/member-logo.png";
+import "./css/home.css";
 
-function Home() {
+export default function Home() {
   return (
     <div>
-      <Jumbotron className="mt-4">
-        <h1>BWI Fingal</h1>
-      </Jumbotron>
-      <Card>
-        <div className="row card-body">
-          <div className="col-sm-8 p-4">
-            <p>
-              Welcome to the Fingal branch of Birdwatch Ireland. Fingal is
-              situated in North Co. Dublin and can boast of great birding spots
-              for the beginner and experienced birder alike. Fingal extends from
-              the outer limits of Dublin city to Meath in the North and to the
-              West.
-            </p>
-            <p>
-              Due to its location on the east coast there is a good variety of
-              coastal habitat to be found including beaches, cliffs, islands and
-              estuaries as well as agricultural land, woodland etc.. Rockabill
-              Island off the coast from Skerries holds one of Europe's largest
-              breeding colonies of Rosate Terns while during the winter months
-              Rogerstown Estuary holds internationally important numbers of
-              Pale-bellied Brent Geese, along with a further 10 species of
-              wintering waterfowl in nationally important numbers, including
-              Wigeon and Black-tailed Godwit.
-            </p>
-            <p>For questions contact bwifingal@gmail.com</p>
-          </div>
-          <img
-            className="col-sm-4"
-            src="https://upload.wikimedia.org/wikipedia/commons/5/50/Island_of_Ireland_location_map_Fingal.svg"
-            alt="Fingal Location"
-          />
-        </div>
-      </Card>
+      <Jumbotron className="jumbo mt-4">{/* <h1>BWI Fingal</h1> */}</Jumbotron>
+
+      <Alert variant="danger">
+        <p>
+          <b>21/10/2020</b>
+        </p>
+        <p>The hide in Turvey park Rogerstown is CLOSED until December.</p>
+        <p>
+          If you are planning to visit please check
+          http://bwifingal.ie/rogerstown-hides/ to see if a warden has been
+          assigned for that date. We rely on volunteers and it is not always
+          possible to cover every Saturday and Sunday between September and
+          March.
+        </p>
+        <p>
+          {" "}
+          All talks and outings are postponed until further notice. This step
+          has been taken to help to ensure the safety of our visitors and our
+          volunteers.
+        </p>
+      </Alert>
+
+      {/* Main Content */}
+      <CardColumns>
+        <Welcome />
+        <BecomeMember />
+        <WetlandsExperiment />
+        <TwitterFeed />
+      </CardColumns>
+
+      {/* Bottom Content (Max 3 cards) */}
+      {/* <CardDeck>
+
+      </CardDeck> */}
     </div>
   );
 }
 
-export default Home;
+function Welcome() {
+  return (
+    <Card bg="dark" text="light">
+      <Card.Body>
+        <Card.Text>
+          <p>
+            Welcome to the Fingal branch of Birdwatch Ireland. Fingal is
+            situated in North Co. Dublin and can boast of great birding spots
+            for the beginner and experienced birder alike. Fingal extends from
+            the outer limits of Dublin city to Meath in the North and to the
+            West.
+          </p>
+          <p>
+            Due to its location on the east coast there is a good variety of
+            coastal habitat to be found including beaches, cliffs, islands and
+            estuaries as well as agricultural land, woodland etc.. Rockabill
+            Island off the coast from Skerries holds one of Europe's largest
+            breeding colonies of Rosate Terns while during the winter months
+            Rogerstown Estuary holds internationally important numbers of
+            Pale-bellied Brent Geese, along with a further 10 species of
+            wintering waterfowl in nationally important numbers, including
+            Wigeon and Black-tailed Godwit.
+          </p>
+        </Card.Text>
+        <Card.Link href="mailto:bwifingal@gmail.com">
+          <Button variant="info" size="sm" block>
+            For questions contact bwifingal@gmail.com
+          </Button>
+        </Card.Link>
+      </Card.Body>
+      <Card.Img variant="bottom" src={FingalLocation} />
+    </Card>
+  );
+}
+
+function BecomeMember() {
+  return (
+    <Card bg="dark" text="light">
+      <Card.Body>
+        <Card.Text>
+          <p>
+            As a voluntary organisation, the financial support we receive from
+            our members is vital if we are to be able to continue our bird and
+            habitat conservation work, keep working with schools and teachers
+            and create and manage nature reserves. By becoming a member today,
+            you can help us in our work and make a real difference to wildlife
+            in Ireland For more details and an application form visit
+          </p>
+        </Card.Text>
+        <Card.Link href="https://birdwatchireland.ie/get-involved/join-us-become-a-member/">
+          <Button variant="light" block>
+            <Card.Img className="p-4" variant="bottom" src={MemberLogo} />
+            <Button className="mt-2 mb-3" variant="info" size="sm">
+              Become a member of BirdWatch Ireland
+            </Button>
+          </Button>
+        </Card.Link>
+      </Card.Body>
+    </Card>
+  );
+}
+
+function WetlandsExperiment() {
+  return (
+    <Card bg="dark" text="light">
+      <Card.Img variant="top" src={Rogerstown} />
+      <Card.Body>
+        <Card.Title>Let it flood: an Irish wetland experiment</Card.Title>
+        <Card.Text>
+          A remarkable ecological initiative is under way in Rogerstown Estuary,
+          just north of Dublin
+        </Card.Text>
+        <Card.Link href="http://www.irishtimes.com/news/environment/let-it-flood-an-irish-wetland-experiment-1.2435139">
+          <Button variant="info" block>
+            Irish Times article
+          </Button>
+        </Card.Link>
+      </Card.Body>
+    </Card>
+  );
+}
+
+function TwitterFeed() {
+  return (
+    <Card bg="dark" text="light">
+      <Card.Body>
+        <Card.Title>
+          Twitter
+          <div className="float-right">
+            <a
+              href="https:/\/twitter.com/bwifingal?ref_src=twsrc%5Etfw"
+              className=" text-rigth twitter-follow-button"
+              data-show-count="false"
+            >
+              Follow @bwifingal
+            </a>
+            <script
+              async
+              src="https://platform.twitter.com/widgets.js"
+              charSet="utf-8"
+            ></script>
+          </div>
+        </Card.Title>
+        <Card.Text>
+          Follow us over on Twitter where we provide more regular updates and
+          news
+          <br />
+        </Card.Text>
+        <Timeline
+          dataSource={{
+            sourceType: "profile",
+            screenName: "BWIFingal",
+          }}
+          options={{
+            height: 876,
+            theme: "dark",
+          }}
+        />
+      </Card.Body>
+    </Card>
+  );
+}
