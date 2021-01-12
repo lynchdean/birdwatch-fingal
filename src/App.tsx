5c192ab1e7c831cb60fec86f8e44db5ddefdde78
Dean <lynchdean@gmail.com>
2021-01-12 00:04:38 +0000

    config

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
-import { Navbar, Nav, Container } from "react-bootstrap";
+import { Navbar, Nav } from "react-bootstrap";
 import Home from "./pages/Home";
 import About from "./pages/About";
 import brand from "./assets/brand.png";
