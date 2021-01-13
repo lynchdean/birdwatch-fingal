7012a77b8ade2d43e0e524a76da471daebc36625
Dean <lynchdean@gmail.com>
2021-01-13 22:59:28 +0000

    Break up content into sep cards

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import React from "react";
-import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import { Navbar, Nav } from "react-bootstrap";
+import { LinkContainer } from "react-router-bootstrap";
 import Home from "./pages/Home";
 import About from "./pages/About";
 import brand from "./assets/brand.png";
@@ -9,55 +10,68 @@ import Hides from "./pages/Hides";
 
 export default function App() {
   return (
-    <Router>
-      <Navbar bg="dark" variant="dark" expand="md">
-        <Navbar.Brand href="/">
-          <img
-            alt="BirdWatch Fingal"
-            src={brand}
-            width="200"
-            className="d-inline-block align-top my-2 mr-4"
-          />{" "}
-        </Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="mr-auto">
-            <Navbar.Text className="mr-3">
-              <Link to="/">Home</Link>
-            </Navbar.Text>
-            <Navbar.Text className="mr-3">
-              <Link to="/about">About</Link>
-            </Navbar.Text>
-            <Navbar.Text className="mr-3">
-              <Link to="/hides">Rogerstown Hides</Link>
-            </Navbar.Text>
-            <Navbar.Text className="mr-3">
-              <Link to="/iWebs">iWebs</Link>
-            </Navbar.Text>
-          </Nav>
-        </Navbar.Collapse>
-      </Navbar>
+    <div className="d-flex flex-column min-vh-100">
+      <Router>
+        <Navbar bg="dark" variant="dark" expand="md">
+          <Navbar.Brand href="/">
+            <img
+              alt="BirdWatch Fingal"
+              src={brand}
+              width="200"
+              className="d-inline-block align-top my-2 mr-4"
+            />{" "}
+          </Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="mr-auto">
+              <LinkContainer to="/">
+                <Nav.Link>Home</Nav.Link>
+              </LinkContainer>
 
-      <Switch>
-        <Route path="/iwebs">
-          <IWebs />
-        </Route>
-        <Route path="/hides">
-          <Hides />
-        </Route>
-        <Route path="/about">
-          <About />
-        </Route>
-        <Route path="/">
-          <Home />
-        </Route>
-      </Switch>
+              <LinkContainer to="/about">
+                <Nav.Link>About</Nav.Link>
+              </LinkContainer>
+              <LinkContainer to="/hides">
+                <Nav.Link>Rogerstown Hides</Nav.Link>
+              </LinkContainer>
 
-      <nav className="navbar sticky-bottom navbar-dark bg-dark text-light">
-        <div className="navbar-text mx-auto">
-          <small>© 2020. Bird Watch Ireland. All Rights Reserved.</small>
+              <LinkContainer to="/iWebs">
+                <Nav.Link>I-Webs</Nav.Link>
+              </LinkContainer>
+              {/* <Navbar.Text className="mr-3">
+                <Link to="/about">About</Link>
+              </Navbar.Text>
+              <Navbar.Text className="mr-3">
+                <Link to="/hides">Rogerstown Hides</Link>
+              </Navbar.Text>
+              <Navbar.Text className="mr-3">
+                <Link to="/iWebs">iWebs</Link>
+              </Navbar.Text> */}
+            </Nav>
+          </Navbar.Collapse>
+        </Navbar>
+        <div className="flex-fill">
+          <Switch>
+            <Route path="/iwebs">
+              <IWebs />
+            </Route>
+            <Route path="/hides">
+              <Hides />
+            </Route>
+            <Route path="/about">
+              <About />
+            </Route>
+            <Route path="/">
+              <Home />
+            </Route>
+          </Switch>
         </div>
-      </nav>
-    </Router>
+        <nav className="navbar navbar-fixed-bottom navbar-dark bg-dark text-light">
+          <div className="navbar-text mx-auto">
+            <small>© 2020. Bird Watch Ireland. All Rights Reserved.</small>
+          </div>
+        </nav>
+      </Router>
+    </div>
   );
 }
--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -1,9 +1,9 @@
 import React from "react";
-import { Card, Container, ListGroup } from "react-bootstrap";
+import { Card, CardColumns, Container, ListGroup } from "react-bootstrap";
 
 function About() {
   return (
-    <Container className="py-4">
+    <Container className="py-4 mh-100">
       <Card bg="dark" text="light">
         <Card.Body>
           <Card.Text>
@@ -18,96 +18,98 @@ function About() {
         </Card.Body>
       </Card>
 
-      <Card className="mt-4" bg="dark" text="light">
-        <Card.Header>
-          <h5>Surveys:</h5>
-        </Card.Header>
-        <ListGroup variant="flush">
-          <ListGroup.Item className="bg-dark">
-            <a href="http://www.bwifingal.ie/i-webs/" className="text-light ">
-              I-WeBS
-            </a>
-          </ListGroup.Item>
-          <ListGroup.Item className="bg-dark">
-            <a
-              href="https://birdwatchireland.ie/our-work/surveys-research/research-surveys/countryside-bird-survey/"
-              className="text-light "
-            >
-              Countryside Bird Survey
-            </a>
-          </ListGroup.Item>
-          <ListGroup.Item className="bg-dark">
-            <a
-              href="https://birdwatchireland.ie/our-work/surveys-research/research-surveys/irish-garden-bird-survey/"
-              className="text-light "
-            >
-              Garden Bird Survey
-            </a>
-          </ListGroup.Item>
-          <ListGroup.Item className="bg-dark">
-            {/* <a href="#" className="text-light "> */}
-            Lapwing Breeding Survey - Rogerstown
-            {/* </a> */}
-          </ListGroup.Item>
-          <ListGroup.Item className="bg-dark">
-            <a
-              href="http://www.bwifingal.ie/birds/little-tern/"
-              className="text-light "
-            >
-              Little Tern Breeding Survey - Portrane
-            </a>
-          </ListGroup.Item>
-        </ListGroup>
-      </Card>
+      <CardColumns>
+        <Card className="mt-4" bg="dark" text="light">
+          <Card.Header>
+            <h5>Surveys:</h5>
+          </Card.Header>
+          <ListGroup variant="flush">
+            <ListGroup.Item className="bg-dark">
+              <a href="http://www.bwifingal.ie/i-webs/" className="text-light ">
+                I-WeBS
+              </a>
+            </ListGroup.Item>
+            <ListGroup.Item className="bg-dark">
+              <a
+                href="https://birdwatchireland.ie/our-work/surveys-research/research-surveys/countryside-bird-survey/"
+                className="text-light "
+              >
+                Countryside Bird Survey
+              </a>
+            </ListGroup.Item>
+            <ListGroup.Item className="bg-dark">
+              <a
+                href="https://birdwatchireland.ie/our-work/surveys-research/research-surveys/irish-garden-bird-survey/"
+                className="text-light "
+              >
+                Garden Bird Survey
+              </a>
+            </ListGroup.Item>
+            <ListGroup.Item className="bg-dark">
+              {/* <a href="#" className="text-light "> */}
+              Lapwing Breeding Survey - Rogerstown
+              {/* </a> */}
+            </ListGroup.Item>
+            <ListGroup.Item className="bg-dark">
+              <a
+                href="http://www.bwifingal.ie/birds/little-tern/"
+                className="text-light "
+              >
+                Little Tern Breeding Survey - Portrane
+              </a>
+            </ListGroup.Item>
+          </ListGroup>
+        </Card>
 
-      <Card className="mt-4" bg="dark" text="light">
-        <Card.Header>
-          <h5>Events:</h5>
-        </Card.Header>
-        <Card.Body>
-          <Card.Text>
-            <p>
-              We host a series of indoor and outdoor events throughout the year.
-              See our Events page for more details. These events are open to the
-              public and free of charge with an option to make a small donation
-              at the end. We regularly support joint events with organisations
-              such Fingal County Council, local schools, tidy town groups and
-              other societies, giving talks or walks.
-            </p>
-          </Card.Text>
-        </Card.Body>
-      </Card>
+        <Card className="mt-4" bg="dark" text="light">
+          <Card.Header>
+            <h5>Events:</h5>
+          </Card.Header>
+          <Card.Body>
+            <Card.Text>
+              <p>
+                We host a series of indoor and outdoor events throughout the
+                year. See our Events page for more details. These events are
+                open to the public and free of charge with an option to make a
+                small donation at the end. We regularly support joint events
+                with organisations such Fingal County Council, local schools,
+                tidy town groups and other societies, giving talks or walks.
+              </p>
+            </Card.Text>
+          </Card.Body>
+        </Card>
 
-      <Card className="mt-4" bg="dark" text="light">
-        <Card.Header>
-          <h5>Hides:</h5>
-        </Card.Header>
-        <Card.Body>
-          <Card.Text>
-            <p>
-              We maintain two hides at Rogerstown estuary. See our Rogerstown
-              Hides page for more details.
-            </p>
-          </Card.Text>
-        </Card.Body>
-      </Card>
+        <Card className="mt-4" bg="dark" text="light">
+          <Card.Header>
+            <h5>Hides:</h5>
+          </Card.Header>
+          <Card.Body>
+            <Card.Text>
+              <p>
+                We maintain two hides at Rogerstown estuary. See our Rogerstown
+                Hides page for more details.
+              </p>
+            </Card.Text>
+          </Card.Body>
+        </Card>
 
-      <Card className="mt-4" bg="dark" text="light">
-        <Card.Header>
-          <h5>Fundraising:</h5>
-        </Card.Header>
-        <Card.Body>
-          <Card.Text>
-            <p>
-              We rely on the support of the public to finance our activities,
-              such as the maintenance of the hides at Rogerstown and running the
-              Little Tern and Lapwing projects. Our main fundraiser is the
-              annual General Knowledge Pub Quiz in January every year so your
-              support is much welcomed.
-            </p>
-          </Card.Text>
-        </Card.Body>
-      </Card>
+        <Card className="mt-4" bg="dark" text="light">
+          <Card.Header>
+            <h5>Fundraising:</h5>
+          </Card.Header>
+          <Card.Body>
+            <Card.Text>
+              <p>
+                We rely on the support of the public to finance our activities,
+                such as the maintenance of the hides at Rogerstown and running
+                the Little Tern and Lapwing projects. Our main fundraiser is the
+                annual General Knowledge Pub Quiz in January every year so your
+                support is much welcomed.
+              </p>
+            </Card.Text>
+          </Card.Body>
+        </Card>
+      </CardColumns>
     </Container>
   );
 }
