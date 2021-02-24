c13526ccf7d50f6cb969657b5fa3737cd47832c6
Dean <lynchdean@gmail.com>
2021-02-24 18:09:21 +0000

    Add links content

--- src/pages/Links.tsx
+++ src/pages/Links.tsx
@@ -1,6 +1,23 @@
 import React from "react";
-import { Container } from "react-bootstrap";
+import { Card, Container, ListGroup } from "react-bootstrap";
+import data from "./data/links";
 
 export default function Links() {
-  return <Container className="">Links</Container>;
+  return (
+    <Container>
+      <Card className="mt-4" bg="dark" text="light">
+        <ListGroup variant="flush">
+          {data.links.map((link: any, index: number) => {
+            return (
+              <ListGroup.Item className="bg-dark" key={index}>
+                <a href={link.uri} className="text-light ">
+                  {link.title}
+                </a>
+              </ListGroup.Item>
+            );
+          })}
+        </ListGroup>
+      </Card>
+    </Container>
+  );
 }
---
+++ src/pages/data/links.js
@@ -0,0 +1,55 @@
+const data = {
+  links: [
+    { title: "Birdwatch Ireland", uri: "https://birdwatchireland.ie/" },
+    {
+      title: "Other Branches",
+      uri: "https://birdwatchireland.ie/our-work/local-branches/",
+    },
+    {
+      title: "National Parks and Wildlife Service",
+      uri: "https://www.npws.ie/",
+    },
+    {
+      title: "Duchas",
+      uri: "https://www.duchas.ie/en",
+    },
+    {
+      title: "Irish Wildlife Trust",
+      uri: "https://iwt.ie/",
+    },
+    {
+      title: "Irish Raptor Study Group",
+      uri: "http://irsg.ie/irsg.html#!/",
+    },
+    {
+      title: "The Golden Eagle Trust",
+      uri: "http://www.goldeneagle.ie/",
+    },
+    {
+      title: "Irish Rare Birds Committee",
+      uri: "http://www.irbc.ie/",
+    },
+    {
+      title: "",
+      uri: "",
+    },
+    {
+      title: "RSPB",
+      uri: "https://www.rspb.org.uk/",
+    },
+    {
+      title: "Northern Ireland",
+      uri: "http://nibirds.blogspot.com/",
+    },
+    {
+      title: "Scotland",
+      uri: "https://www.the-soc.org.uk/",
+    },
+    {
+      title: "Wales",
+      uri: "https://birdsin.wales/",
+    },
+  ],
+};
+
+export default data;
