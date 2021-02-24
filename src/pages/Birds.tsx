9649c524fa7c6ead5b95194cf4ac1cbf3a45321c
Dean <lynchdean@gmail.com>
2021-02-24 18:17:37 +0000

    Add page placeholders

--- src/pages/Birds.tsx
+++ src/pages/Birds.tsx
@@ -1,6 +1,11 @@
 import React from "react";
 import { Container } from "react-bootstrap";
+import DarkCard from "../components/DarkCard";
 
 export default function Birds() {
-  return <Container className="">Birds</Container>;
+  return (
+    <Container className="">
+      <DarkCard title="Birds" text="Content to be added soon" />
+    </Container>
+  );
 }
--- src/pages/Sites.tsx
+++ src/pages/Sites.tsx
@@ -1,6 +1,11 @@
 import React from "react";
 import { Container } from "react-bootstrap";
+import DarkCard from "../components/DarkCard";
 
 export default function Sites() {
-  return <Container className="">Sites</Container>;
+  return (
+    <Container className="">
+      <DarkCard title="Sites" text="Content to be added soon" />
+    </Container>
+  );
 }
