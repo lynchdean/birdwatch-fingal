c3e8e3d14139f133c57aae8d3a6b2016931c9a13
Dean <lynchdean@gmail.com>
2021-04-25 21:01:36 +0100

    Add pdfs to site

--- src/pages/Sites.tsx
+++ src/pages/Sites.tsx
@@ -1,11 +1,23 @@
 import React from "react";
-import { Container } from "react-bootstrap";
-import DarkCard from "../components/DarkCard";
+import { Card, Container, ListGroup } from "react-bootstrap";
 
 export default function Sites() {
   return (
-    <Container className="">
-      <DarkCard title="Sites" text="Content to be added soon" />
-    </Container>
+    <Container>
+    <Card className="mt-4" bg="dark" text="light">
+      <Card.Header><h5>Site Guides</h5></Card.Header>
+      <ListGroup variant="flush">
+            <ListGroup.Item className="bg-dark">
+              <a href="/Fingal-sites.pdf" className="text-light">Where to watch, North Fingal (Wings 2010)</a>
+            </ListGroup.Item>
+            <ListGroup.Item className="bg-dark">
+              <a href="/Wings-Feb-2015-Broadmeadow.pdf" className="text-light">Broadmeadow Estuary (Wings Feb 2015)</a>
+            </ListGroup.Item>
+            <ListGroup.Item className="bg-dark">
+              <a href="/Wings-feb2011-Fingal_hr.pdf" className="text-light">Fingal Site guide (Wings Feb 2011))</a>
+            </ListGroup.Item>
+      </ListGroup>
+    </Card>
+  </Container>
   );
 }
