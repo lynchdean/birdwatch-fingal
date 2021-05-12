8ac2c26495b35c65ae757833d60721c2eab9d5de
Dean <lynchdean@gmail.com>
2021-05-12 22:52:47 +0100

    Cahnge card order

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -30,9 +30,9 @@ export default function Home() {
         <Welcome />
         <CardColumns>
           <BecomeMember />
+          <YouTube/>
           <WetlandsExperiment />
           <TwitterFeed />
-          <YouTube/>
         </CardColumns>
       </Container>
     </div>
