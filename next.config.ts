9609a664f8fd434da52150a1be65acdd56fcb18f
Dean Lynch <lynchdean@gmail.com>
2025-05-11 16:01:23 +0100

    redirect /brids to /terns

--- next.config.ts
+++ next.config.ts
@@ -4,8 +4,16 @@ const nextConfig: NextConfig = {
     output: "export",
     images: {
         unoptimized: true
+    },
+    async redirects() {
+        return [
+            {
+                source: '/birds',
+                destination: '/terns',
+                permanent: true,
+            },
+        ]
     }
 };
-
 
 export default nextConfig;
